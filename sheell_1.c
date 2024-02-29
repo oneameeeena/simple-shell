c4e130b55891d296e8dfa66b42626f494a4951a4
oneameeeena <aminaouaqqaourahou@gmail.com>
2024-02-29 15:37:06 -0800

    am/sh

--- sheell_1.c
+++ sheell_1.c
@@ -1,59 +1,143 @@
-#include "SHELL.H"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
+#include"shell.h"
+/**
+ * strcat_cd - function concatenates error
+ *
+ * @Data_sh: data (directory)
+ * @msg: message
+ * @error: stdout msg
+ * @ver_str: ln_count lines
+ * Return: error
+ */
+char *strcat_cd(Data_sl *Data_sh, char *msg, char *error, char *ver_str)
+{
+	char *NoPermited_flag;
 
-#define MAX_LINE_LENGTH 1024
+	_strcpy(error, Data_sh->av[0]);
+	_strcat(error, ": ");
+	_strcat(error, ver_str);
+	_strcat(error, ": ");
+	_strcat(error, Data_sh->args[0]);
+	_strcat(error, msg);
+	if (Data_sh->args[1][0] == '-')
+	{
+		NoPermited_flag = malloc(3);
+		NoPermited_flag[0] = '-';
+		NoPermited_flag[1] = Data_sh->args[1][1];
+		NoPermited_flag[2] = '\0';
+		_strcat(error, NoPermited_flag);
+		free(NoPermited_flag);
+	}
+	else
+	{
+		_strcat(error, Data_sh->args[1]);
+	}
 
-void execute_command(char *command) {
-	char *argv[MAX_LINE_LENGTH/2 + 1];
-	int argc = 0;
-	char *token = strtok(command, " ");
-
-	while (token != NULL) {
-	argv[argc++] = token;
-	token = strtok(NULL, " ");
-    }
-    argv[argc] = NULL;
-
-	pid_t pid = fork();
-	if (pid == 0) {
-	if (execvp(argv[0], argv) == -1) {
-	perror("Error executing command");
-        }
-        exit(EXIT_FAILURE);
-    } else if (pid < 0) {
-        perror("Error forking");
-    } else {
-
-	    int status;
-	    waitpid(pid, &status, 0);
-    }
+	_strcat(error, "\n");
+	_strcat(error, "\0");
+	return (error);
 }
 
-int main() {
-	char *line;
-	size_t len = 0;
-	ssize_t read;
-	
-	while (1) {
-	printf("#cisfun$ ");
-	fflush(stdout);
+/**
+ * error_get_cd - error message to cmd get_cd
+ * @Data_sh: data directory
+ * Return: Error
+ */
+char *error_get_cd(Data_sl *Data_sh)
+{
+	int lent, idLent;
+	char *error, *ver_str, *msg;
 
-	read = getline(&line, &len, stdin);
-	if (read == -1) {
-		break;
+	ver_str = func_itoa(Data_sh->ln_count);
+	if (Data_sh->args[1][0] == '-')
+	{
+		msg = ": Illegal option ";
+		idLent = 2;
+	}
+	else
+	{
+		msg = ": can't cd to ";
+		idLent = _strlen(Data_sh->args[1]);
 	}
 
-	if (strcmp(line, "exit\n") == 0) {
-		break;
+	lent = _strlen(Data_sh->av[0]) + _strlen(Data_sh->args[0]);
+	lent += _strlen(ver_str) + _strlen(msg) + idLent + 5;
+	error = malloc(sizeof(char) * (lent + 1));
+
+	if (error == 0)
+	{
+		free(ver_str);
+		return (NULL);
 	}
 
-	execute_command(line);
-	}
+	error = strcat_cd(Data_sh, msg, error, ver_str);
 
-	free(line);
-	return 0;
+	free(ver_str);
+
+	return (error);
 }
 
+/**
+ * error_not_found - generic error command not found
+ * @Data_sh: data (ln_count, arguments)
+ * Return: Error
+ */
+char *error_not_found(Data_sl *Data_sh)
+{
+	int lent;
+	char *error;
+	char *ver_str;
+
+	ver_str = func_itoa(Data_sh->ln_count);
+	lent = _strlen(Data_sh->av[0]) + _strlen(ver_str);
+	lent += _strlen(Data_sh->args[0]) + 16;
+	error = malloc(sizeof(char) * (lent + 1));
+	if (error == 0)
+	{
+		free(error);
+		free(ver_str);
+		return (NULL);
+	}
+	_strcpy(error, Data_sh->av[0]);
+	_strcat(error, ": ");
+	_strcat(error, ver_str);
+	_strcat(error, ": ");
+	_strcat(error, Data_sh->args[0]);
+	_strcat(error, ": not found\n");
+	_strcat(error, "\0");
+	free(ver_str);
+	return (error);
+}
+
+/**
+ * error_exit_shell - generic error exit
+ * @Data_sh: data (ln_count, arguments)
+ *
+ * Return: Error
+ */
+char *error_exit_shell(Data_sl *Data_sh)
+{
+	int lent;
+	char *error;
+	char *ver_str;
+
+	ver_str = func_itoa(Data_sh->ln_count);
+	lent = _strlen(Data_sh->av[0]) + _strlen(ver_str);
+	lent += _strlen(Data_sh->args[0]) + _strlen(Data_sh->args[1]) + 23;
+	error = malloc(sizeof(char) * (lent + 1));
+	if (error == 0)
+	{
+		free(ver_str);
+		return (NULL);
+	}
+	_strcpy(error, Data_sh->av[0]);
+	_strcat(error, ": ");
+	_strcat(error, ver_str);
+	_strcat(error, ": ");
+	_strcat(error, Data_sh->args[0]);
+	_strcat(error, ": Illegal number: ");
+	_strcat(error, Data_sh->args[1]);
+	_strcat(error, "\n\0");
+	free(ver_str);
+
+	return (error);
+}
